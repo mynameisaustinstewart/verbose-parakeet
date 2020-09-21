8516b4abce3fccf854d65a835cdc10aeaf009cf3
mynameisaustinstewart <austin.stewart@mslgroup.com>
2020-09-21 16:44:42 -0700

    First Commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,33 +2,71 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header
+const Header = () => (
+  <div
     style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
+      background: "rebeccapurple",
+      marginBottom: "1.45rem"
     }}
   >
     <div
       style={{
-        margin: `0 auto`,
+        margin: "0 auto",
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: "1.45rem 1.0875rem"
       }}
     >
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            color: "white",
+            textDecoration: "none"
           }}
         >
-          {siteTitle}
+          Gatsby
         </Link>
+        <ul style={{ listStyle: "none", float: "right" }}>
+          <li style={{ display: "inline-block", marginRight: "1rem" }}>
+            <Link
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontSize: "x-large"
+              }}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li style={{ display: "inline-block", marginRight: "1rem" }}>
+            <Link
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontSize: "x-large"
+              }}
+              to="/page-2"
+            >
+              Page 2
+            </Link>
+          </li>
+          <li style={{ display: "inline-block", marginRight: "1rem" }}>
+            <Link
+              style={{
+                color: "white",
+                textDecoration: "none",
+                fontSize: "x-large"
+              }}
+              to="/page-3"
+            >
+              Page 3
+            </Link>
+          </li>
+        </ul>
       </h1>
     </div>
-  </header>
+  </div>
 )
 
 Header.propTypes = {
